5bb8993392173012a5e61cd47543672402613686
ffthkrdg <ffthkrdg@gmail.com>
2020-08-10 23:58:56 +0300

    sidebar complated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
 
 function App() {
   return (
     <div className="app">
-      <h1>Twitter Clone 🚀</h1>
-
-      {/*Sidebar*/}
       <Sidebar />
-      {/*Feed*/}
+      <Feed />
       {/*Widgets*/}
     </div>
   );
---
+++ src/Feed.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <div className="Feed">
+      {/*Header*/}
+      <h1>Home</h1>
+      {/*Tweetbox*/}
+      {/*Post*/}
+    </div>
+  );
+}
+
+export default Feed;
