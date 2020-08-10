29ce3b84988ecf9f9afb1dd27d56a395218a2b0f
ffthkrdg <ffthkrdg@gmail.com>
2020-08-10 23:36:34 +0300

    sidebar complated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Twitter Clone 🚀</h1>
+
+      {/*Sidebar*/}
+      <Sidebar />
+      {/*Feed*/}
+      {/*Widgets*/}
     </div>
   );
 }
